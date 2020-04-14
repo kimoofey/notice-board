0d0ae2660da55068bbc3ac8a0bd608bb09bc223e
Andrey Kim <kimoofey@gmail.com>
2020-04-14 22:49:36 +0300

    wip. creating main components

--- app.js
+++ app.js
@@ -4,20 +4,20 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require('./backend/routes');
+var usersRouter = require('./backend/routes/users');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'backend/views'));
 app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'backend/public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
