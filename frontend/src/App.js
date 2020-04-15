bd345c825061a22e4d5c61bc0eb3543aed231165
Andrey Kim <kimoofey@gmail.com>
2020-04-15 23:46:02 +0300

    wip

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,11 @@
 import React, {Component} from 'react';
 import './App.css';
 import MainPage from "./Components/MainPage/MainPage";
-import Navigation from "./Components/Navigation/Navigation";
 
 class App extends Component {
     render() {
         return (
             <div className="App">
-                <Navigation/>
                 <MainPage/>
             </div>
         );
