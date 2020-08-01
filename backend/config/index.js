753678745b35c11630c77531fa1d28df931da6ec
Andrey Kim <kimoofey@gmail.com>
2020-08-01 12:38:44 +0300

    Connect backend to firebase (#27)
    * feat: connect firebase and express

--- backend/config/index.js
+++ backend/config/index.js
@@ -4,4 +4,7 @@ exports.CLIENT_ORIGIN = process.env.NODE_ENV === 'production'
     ? process.env.CLIENT_ORIGIN
     : 'http://localhost:3000';
 
-exports.SECRET_KEY = 'secret';
+exports.SECRET_KEY = process.env.NODE_ENV === 'production'
+    ? process.env.JWTSECRETKEY
+    : require('./jwtSecretKey.json').jwtSecretKey;
+
